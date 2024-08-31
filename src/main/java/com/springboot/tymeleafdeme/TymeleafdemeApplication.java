56ae37d987b815b1d8dfecb28ef67027931589de
Simovanev <simovanev@abv.bg>
2024-08-31 06:31:23 +0300

    Initial commit

---
+++ src/main/java/com/springboot/tymeleafdeme/TymeleafdemeApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.tymeleafdeme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TymeleafdemeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TymeleafdemeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/tymeleafdeme/TymeleafdemeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.tymeleafdeme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TymeleafdemeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
