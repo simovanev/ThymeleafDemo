3d99c8dd41d6ea0e87626ce9feeaa878532fb2d1
PC1237 <simovanev@abv.bg>
2024-09-02 12:38:19 +0300

    Use mModel to transfer information.

---
+++ src/main/java/com/springboot/tymeleafdeme/controller/HelloWorldController.java
@@ -0,0 +1,27 @@
+package com.springboot.tymeleafdeme.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloWorldController {
+    @RequestMapping("/showForm")
+    public String showForm() {
+        return "input-form";
+    }
+
+    @RequestMapping("/processForm")
+    public String processForm() {
+        return "hello";
+
+    }
+    @RequestMapping("/processFormTwo")
+    public String shoutLoud(HttpServletRequest request, Model model){
+        String theName=request.getParameter("studentName");
+        model.addAttribute("message", theName.toUpperCase());
+        return "hello";
+    }
+}
