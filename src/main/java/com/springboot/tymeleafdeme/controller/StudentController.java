12405ee079908b64d92c2f7e32da156d7fa4db0b
Simovanev <simovanev@abv.bg>
2024-09-04 04:33:09 +0300

    Enter drop-down list from external source - properties file.

--- src/main/java/com/springboot/tymeleafdeme/controller/StudentController.java
+++ src/main/java/com/springboot/tymeleafdeme/controller/StudentController.java
@@ -1,19 +1,25 @@
 package com.springboot.tymeleafdeme.controller;
 
 import com.springboot.tymeleafdeme.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+    @Value("${contries}")
+    private List<String> countries;
 
     @GetMapping("/showStudentForm")
     public String showStudentForm(Model model) {
         Student student = new Student();
         model.addAttribute("student", student);
+        model.addAttribute("countries", countries);
         return "student-form";
     }
     @PostMapping("/processStudentForm")
